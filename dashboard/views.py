47861905d711f77d7744e759b6dee31575527699
EC2 Default User <ec2-user@ip-172-31-14-135.eu-west-1.compute.internal>
2024-04-02 13:36:51 +0000

    pranav

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .forms import *
 from django.contrib import messages
-
+from django.shortcuts import redirect
 # Create your views here.
 def home(request):
     return render(request,'dashboard/home.html')
@@ -19,3 +19,7 @@ def notes(request):
     notes = Notes.objects.filter(user=request.user)
     context = {'form': form, 'notes': notes}
     return render(request, 'dashboard/notes.html', context)
+    
+def delete_note(request, pk=None):
+    Notes.objects.get(id=pk).delete()
+    return redirect('notes')

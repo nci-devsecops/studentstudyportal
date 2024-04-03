bc0da41ab0fe655f08a562ca252286d9a6ac19ad
EC2 Default User <ec2-user@ip-172-31-14-135.eu-west-1.compute.internal>
2024-04-03 19:06:47 +0000

    validation

--- dashboard/views.py
+++ dashboard/views.py
@@ -116,7 +116,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account Created for {username}!')
-            #return redirect('login')
+            redirect('login')
     else:
         form = UserRegisterForm()
     context = {

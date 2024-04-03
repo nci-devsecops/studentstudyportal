dd675a91532d68fbf0c35226bdd131e6471c6e15
EC2 Default User <ec2-user@ip-172-31-14-135.eu-west-1.compute.internal>
2024-04-03 18:52:39 +0000

    login

--- studentstudyportal/settings.py
+++ studentstudyportal/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'dashboard',
     'crispy_forms',
     'crispy_bootstrap5',
+    
 ]
 
 MIDDLEWARE = [
@@ -126,6 +127,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #STATICFILES_DIRS=[BASE_DIR/'static']
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
+
+
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'

86ca429ab041b0aa776389118a3a15abdcad977f
EC2 Default User <ec2-user@ip-172-31-14-135.eu-west-1.compute.internal>
2024-04-03 10:49:35 +0000

    PQR

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('notes_detail/<int:pk>',
          views.NotesDetailView.as_view(), name="notes-details"),
     path('homework', views.homework, name="homework"),
+    path('update_homework/<int:pk>', views.update_homework, name="update-homework"),
+    path('delete_homework/<int:pk>', views.delete_homework, name="delete-homework"),
     ]
     
     

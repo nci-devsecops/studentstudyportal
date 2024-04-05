01eb0678bb9c4fea31e35114fb4b67e991c9d79f
EC2 Default User <ec2-user@ip-172-31-14-135.eu-west-1.compute.internal>
2024-04-05 00:19:05 +0000

    pip3

--- studentstudyportal/settings.py
+++ studentstudyportal/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from pathlib import Path
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
